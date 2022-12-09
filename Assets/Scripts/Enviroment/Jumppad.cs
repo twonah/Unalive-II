abdcab2754b9f62f1bd88bb9b6412bb43a980540
paiwer <preava_1@hotmail.com>
2022-12-09 15:57:00 +0700

    Spike + Animation for Jump pad

--- Assets/Scripts/Enviroment/Jumppad.cs
+++ Assets/Scripts/Enviroment/Jumppad.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Jumppad : MonoBehaviour
 {
     public float bounce = 15f;
+    [SerializeField] private Animator _anim;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
-        }
-            
+            _anim.SetFloat("Activate", 1);
+        } 
+    }
+
+    void AnimEnd()  //Use this in the animation
+    {
+        _anim.SetFloat("Activate", -1);
     }
 }
---
+++ Assets/Scripts/Enviroment/Spike.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] private float _knockback;
+    [SerializeField] private float _damage;
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            _player.GetComponent<HitPoints>().TakeDamage(_damage);
+
+            Knockback();
+        }
+    }
+
+    private void Knockback()
+    {
+        _player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _knockback, ForceMode2D.Impulse);
+    }
+}
