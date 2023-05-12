5114f7b1bc031922bd092d7b9e195d6e884e8129
Tanatorn Songnatsiri <Kin12825@gmail.com>
2023-05-13 02:40:17 +0700

    Sound Setting 3
    Sound Setting menu is work. But it not share the saved sound to other scene.

---
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip background;
+    public AudioClip playerJump;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
