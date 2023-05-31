22798009da6e55c76261d54de6eda93c5fa1026b
Pitchaporn Boonyakongrat <91454193+paiwer@users.noreply.github.com>
2023-05-31 20:58:31 +0700

    Portal update
    transition when enter portal

--- Assets/Scripts/Enviroment/Portal.cs
+++ Assets/Scripts/Enviroment/Portal.cs
@@ -5,25 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField] private int _sceneNumber;
+    [SerializeField] private string _sceneName;
+     private GameObject levelLoader;
+     private GameObject control;
+     private bool isWarp;
+     private GameOver gameOverScript;
+     private PauseMenu pauseMenuScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelLoader = GameObject.FindGameObjectWithTag("LevelLoader");
+        control = GameObject.FindGameObjectWithTag("SwitchControl");
+        gameOverScript = FindObjectOfType<GameOver>();
+        pauseMenuScript = FindObjectOfType<PauseMenu>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player"))
+        if(isWarp && _sceneName != "")
         {
-            SceneManager.LoadScene("level" + _sceneNumber);
+            if(levelLoader.GetComponent<SceneTransition>()._TransitionEnd)
+            {
+                SceneManager.LoadScene(_sceneName);
+                isWarp = false;
+                gameOverScript.enabled = true;
+                pauseMenuScript.enabled = true;
+                control.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            control.SetActive(false);
+            levelLoader.GetComponent<Animator>().SetTrigger("LoadTransition");
+            isWarp = true;
+            gameOverScript.enabled = false;
+            pauseMenuScript.enabled = false;
         }
     }
 }
