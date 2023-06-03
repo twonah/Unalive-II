d70ba4b26fdad2d25da0f25115c8a872bd650aab
Tery Grubb <tgrubb386@gmail.com>
2023-06-03 19:30:54 +0700

    Revive prototype ( just heals dreamform)

--- Assets/Scripts/Enviroment/EnergyBall.cs
+++ Assets/Scripts/Enviroment/EnergyBall.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class EnergyBall : MonoBehaviour
 {
-
+    [SerializeField] HitPoints _DreamForm_HitP;
     [SerializeField] UI_Cooldown _CoolUP;
 
     int EnergyGet = 25;
+    int DreamForm_revive = 1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "EnergyBall")
         {
             _CoolUP._CurrentEnergy += EnergyGet;
+
+            _DreamForm_HitP._CurrentHitPoints += DreamForm_revive; //heals dreamform
+
             Destroy(collision.gameObject);
         }
     }
+
 
 }
